a2c6d848c7926bad336a5675c403b9500e4d1b00
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-03-07 00:05:42 +0530

    adding the API request to the backend and display the response message on the page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const fetchGreeting = async () => {
+    if (!name.trim()) {
+      setMessage("Name is required.");
+      setSuccess(false);
+      return;
+    }
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/api/greet?name=${name}`
+      );
+      setMessage(response.data.message);
+      setSuccess(true);
+    } catch (error) {
+      setMessage("Error fetching greeting.", error);
+      setSuccess(false);
+    }
+  };
 
   return (
     <div className="app-container">
@@ -26,13 +47,37 @@ function App() {
         </div>
 
         <button
-
+          onClick={fetchGreeting}
           className="submit-button"
         >
           Get Greeting
         </button>
 
-       
+        {message && (
+          <div
+            className={`message-container ${
+              success ? "success-message" : "error-message"
+            }`}
+          >
+            {success ? (
+              <div className="status-icon success-icon">
+                ✓
+              </div>
+            ) : (
+              <div className="status-icon error-icon">
+                ✕
+              </div>
+            )}
+            <div className="message-content">
+              {success ? (
+                <div className="message-title">Success</div>
+              ) : (
+                <div className="message-title">Error</div>
+              )}
+              <div>{message}</div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
